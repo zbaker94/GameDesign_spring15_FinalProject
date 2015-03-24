165457b349cb24c382ce1fbe3f3d71ce4d91b988
Zack Baker <zbaker94@gmail.com>
2015-03-23 23:33:26 -0400

    Started adding subclasses, made backgrounds for 3 countries

---
+++ Scenario/DESTROYING THE GREAT CATHEDRALS OF EUROPE/France.java
@@ -0,0 +1,19 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class France here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class France extends Country
+{
+
+    /**
+     * Constructor for objects of class France.
+     * 
+     */
+    public France()
+    {
+    }
+}
