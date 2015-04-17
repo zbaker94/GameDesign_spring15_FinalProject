866e76b495aec9f8f551dbfa0fe0e3aa071987dd
Brett Wallace <bwalla989@gmail.com>
2015-04-17 10:48:23 -0400

    Bullets rotate correctly now

--- Scenario/DESTROYING THE GREAT CATHEDRALS OF EUROPE/EnemyBullet.java
+++ Scenario/DESTROYING THE GREAT CATHEDRALS OF EUROPE/EnemyBullet.java
@@ -11,14 +11,18 @@ public class EnemyBullet extends Bullet
     int speed = 8;
     public EnemyBullet(String direction){
         super(direction);
+        if(direction == "up" || direction == "down") {
+            turn(90);
     }
+}
+
     /**
      * Act - do whatever the EnemyBullet wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-         moveDir(speed);
+        moveDir(speed);
         if(atWorldEdge()){
             World f = getWorld();
             getWorld().removeObject(this);
@@ -26,19 +30,19 @@ public class EnemyBullet extends Bullet
         }
         checkCollision();
     }    
-     public void checkCollision()
+
+    public void checkCollision()
     {
         Player p =(Player)getOneIntersectingObject(Player.class);
         Cathedral c = (Cathedral)getOneIntersectingObject(Cathedral.class);
         if (p != null){
-       Greenfoot.playSound("hit_enemy.wav");
-       p.die();
-        getWorld().removeObject(this);  // Removes the Shot
+            Greenfoot.playSound("hit_enemy.wav");
+            p.die();
+            getWorld().removeObject(this);  // Removes the Shot
+        }
+        //     if(c != null){
+        //         getWorld().removeObject(this); // Removes the Shot
+        //     }
     }
-//     if(c != null){
-//         getWorld().removeObject(this); // Removes the Shot
-//     }
 }
-}
-
 
